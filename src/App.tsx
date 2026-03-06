39aaf65da14114a7ebfa781350bdb103f8b4b4f9
Renata Sanchez <rdbsanchez@gmail.com>
2026-03-05 22:13:47 -0300

    conteudo da pagina de inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import {Container} from "reactstrap";
+import {Col, Container, Row} from "reactstrap";
 import bannerPhoto from "../src/assets/photo_banner2.jpg";
+import nossaFoto from "../src/assets/nossa-foto.jpg";
 import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
 import "@leenguyen/react-flip-clock-countdown/dist/index.css";
 
@@ -22,6 +23,25 @@ function App() {
           className="flip-custom"
         />
       </div>
+
+      <Row>
+        <Col md="8" className="bg--blue p-3">
+
+          <img src={nossaFoto} alt="Foto do casal" className="foto-casal"/>
+
+        </Col>
+        <Col md="4" className="bg--blue text-white p-3 d-flex align-items-center">
+          <p>
+            Oii!
+            <br /><br />
+            Ficaremos muito felizes em ter você presente no nosso grande dia e mal podemos esperar para compartilhar esse momento tão especial ao seu lado!
+            <br /><br />
+            Aqui no nosso site você encontrará nossa lista de presentes, dicas e informações sobre o local do casamento, além de um espaço para confirmar sua presença.
+            <br /><br />
+            Sua presença tornará esse dia ainda mais especial para nós!
+          </p>
+        </Col>
+      </Row>
     </Container>
   );
 }
