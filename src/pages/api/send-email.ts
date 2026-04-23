ce5d8aa81674a2eb283ef704a5b4e3ca12bcf0ab
Renata Sanchez <rdbsanchez@gmail.com>
2026-04-23 17:29:31 -0300

    tentativa de correção de api

--- src/pages/api/send-email.ts
+++ src/pages/api/send-email.ts
@@ -1,7 +1,11 @@
 export default async function handler(req: any, res: any) {
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
+
 
   try {
     const {
